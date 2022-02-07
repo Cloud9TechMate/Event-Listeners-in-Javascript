b8d02a85a99828e39e57b708ac6d0bd83acf3e63
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-02-06 19:10:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -163,14 +163,10 @@ document.addEventListener("readystatechange", (event) => {
 })
 
 const initApp = () => {
-    const view3 = document.querySelector("#view3");
-    const myForm = view3.querySelector("#myForm");
+    // const view3 = document.querySelector("#view3");
+    // const myForm = view3.querySelector("#myForm");
     myForm.addEventListener("submit", (event) => {
         event.preventDefault();
-        console.log("submit event");
+        console.log("submit event"); alert("hello")
     });
 };
-
-
-
-
